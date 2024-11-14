5da92a30f6e8f41a73f31d2d653c615b42649bd1
cetusw <kugelev.m@gmail.com>
2024-11-14 20:03:58 +0300

    Дописал колизию

--- main.cpp
+++ main.cpp
@@ -1,19 +1,25 @@
-#include <cmath>
 #include <iostream>
 #include <random>
 
 #include <SFML/Graphics.hpp>
+
+struct Size
+{
+    float width;
+    float height;
+};
 
 constexpr unsigned WINDOW_WIDTH = 1920;
 constexpr unsigned WINDOW_HEIGHT = 1080;
 
 constexpr float FREE_FALL_ACCELERATION = 3.7f;
 constexpr float MAX_HOPPER_VELOCITY = 500.0f;
-
-struct Size {
-    float width;
-    float height;
-};
+constexpr Size HOPPER_SIZE = {150.f, 150.f};
+constexpr Size PLATFORM_SIZE = {400.f, 50.f};
+const sf::Vector2f START_PLATFORM_POSITION = {100.f, 800.f};
+const sf::Vector2f FIRST_PLATFORM_POSITION = {600.f, 400.f};
+const sf::Vector2f SECOND_PLATFORM_POSITION = {1500.f, 800.f};
+const sf::Vector2f START_HOPPER_POSITION = {300.f, 700.f};
 
 struct Hopper
 {
@@ -21,14 +27,14 @@ struct Hopper
     sf::Vector2f velocity = {0, 0};
     sf::Vector2f acceleration = {0, 0};
     sf::Vector2f position;
-    Size size = {150.f, 150.f};
+    Size size = HOPPER_SIZE;
 };
 
 struct Platform
 {
     sf::ConvexShape body;
     sf::Vector2f position;
-    Size size = {400.f, 50.f};
+    Size size = PLATFORM_SIZE;
 };
 
 float getRandomNumber(const float min, const float max)
@@ -48,7 +54,8 @@ void loadTexture(sf::Texture &texture, const std::string &filePath)
     }
 }
 
-sf::Vector2f calculateHopperSize(const Hopper &hopper, const sf::Texture &texture) {
+sf::Vector2f calculateHopperSize(const Hopper &hopper, const sf::Texture &texture)
+{
     const sf::Vector2u textureSize = texture.getSize();
     return {
         hopper.size.width / static_cast<float>(textureSize.x),
@@ -63,28 +70,14 @@ void initHopper(Hopper &hopper, sf::Texture &texture, const std::string &filePat
     const sf::Vector2f hopperSize = calculateHopperSize(hopper, texture);
 
     hopper.body.setScale(hopperSize);
-    hopper.position = {400, 300};
+    hopper.position = START_HOPPER_POSITION;
     hopper.body.setTexture(texture);
     hopper.body.setOrigin(sf::Vector2f(0, 0));
 }
 
-void initPlatform(Platform &platform)
+void initPlatform(Platform &platform, float x, float y)
 {
-    platform.position = {getRandomNumber(0, WINDOW_WIDTH), getRandomNumber(0, WINDOW_HEIGHT)};
-    platform.body.setPosition(platform.position);
-
-    platform.body.setPointCount(4);
-    platform.body.setPoint(0, {0, 0});
-    platform.body.setPoint(1, {platform.size.width, 0});
-    platform.body.setPoint(2, {platform.size.width, platform.size.height});
-    platform.body.setPoint(3, {0, platform.size.height});
-
-    platform.body.setFillColor(sf::Color(0xFF, 0xFF, 0xFF));
-}
-
-void initStartPlatform(Platform &platform)
-{
-    platform.position = {400, 300};
+    platform.position = {x, y};
     platform.body.setPosition(platform.position);
 
     platform.body.setPointCount(4);
@@ -101,29 +94,22 @@ void initPlatforms(std::vector<Platform> &platform)
     Platform startPlatform, platform1, platform2;
     platform.emplace_back(platform1);
     platform.emplace_back(platform2);
-    initStartPlatform(startPlatform);
-    platform.emplace_back(startPlatform);
-    for (Platform &onePlatform : platform)
-    {
-        initPlatform(onePlatform);
-    }
+    initPlatform(platform[0], FIRST_PLATFORM_POSITION.x, FIRST_PLATFORM_POSITION.y);
+    initPlatform(platform[1], SECOND_PLATFORM_POSITION.x, SECOND_PLATFORM_POSITION.y);
 }
 
 void reduceAccelerationX(Hopper &hopper)
 {
-    constexpr float REDUCTION_RATE_X = 2.0f;
-
     if (hopper.acceleration.x > 0)
     {
-        hopper.acceleration.x -= REDUCTION_RATE_X;
+        hopper.acceleration.x -= FREE_FALL_ACCELERATION;
         if (hopper.acceleration.x < 0)
         {
             hopper.acceleration.x = 0;
         }
-    }
-    else if (hopper.acceleration.x < 0)
+    } else if (hopper.acceleration.x < 0)
     {
-        hopper.acceleration.x += REDUCTION_RATE_X;
+        hopper.acceleration.x += FREE_FALL_ACCELERATION;
         if (hopper.acceleration.x > 0)
         {
             hopper.acceleration.x = 0;
@@ -136,8 +122,7 @@ void reduceAccelerationY(Hopper &hopper)
     if (hopper.acceleration.y > 0)
     {
         hopper.acceleration.y -= FREE_FALL_ACCELERATION;
-    }
-    else
+    } else
     {
         hopper.acceleration.y = 0;
     }
@@ -146,33 +131,39 @@ void reduceAccelerationY(Hopper &hopper)
 bool collidedWithPlatform(const Hopper &hopper, const Platform &platform)
 {
     return !(hopper.position.y >= platform.position.y + platform.size.height ||
-        (hopper.position.x + hopper.size.width <= platform.position.x ||
-         hopper.position.x >= platform.position.x + platform.size.width ||
-         hopper.position.y + hopper.size.height <= platform.position.y));
+             (hopper.position.x + hopper.size.width <= platform.position.x ||
+              hopper.position.x >= platform.position.x + platform.size.width ||
+              hopper.position.y + hopper.size.height <= platform.position.y));
 }
 
 void updateCollide(std::vector<Platform> &platforms, Hopper &hopper, const float deltaTime)
 {
     bool collided = false;
 
-    for (Platform &onePlatform : platforms)
+    for (Platform &currentPlatform: platforms)
     {
-        if (collidedWithPlatform(hopper, onePlatform))
+        if (collidedWithPlatform(hopper, currentPlatform))
         {
-            collided = true;
-            hopper.position.y = onePlatform.position.y - hopper.size.height;
-            break;
+            if (hopper.position.y + hopper.size.height <= currentPlatform.position.y + currentPlatform.size.height)
+            {
+                hopper.position.y = currentPlatform.position.y - hopper.size.height;
+                hopper.velocity = {0, 0};
+                collided = true;
+                break;
+            }
         }
     }
-    if (collided)
-    {
-        hopper.velocity = {0, 0};
-        hopper.acceleration = {0, 0};
-    }
-    else
+
+    if (!collided)
     {
         hopper.position += hopper.velocity * deltaTime;
-        std::cout << hopper.position.y << std::endl;
+    }
+
+    if (hopper.position.y > WINDOW_HEIGHT)
+    {
+        hopper.position = START_HOPPER_POSITION;
+        hopper.velocity = {0, 0};
+        hopper.acceleration = {0, 0};
     }
 }
 
@@ -208,18 +199,16 @@ void pollEvents(sf::RenderWindow &window, Hopper &hopper)
                 if (event.key.code == sf::Keyboard::Up)
                 {
                     hopper.acceleration.y += 70.0f;
-                }
-                else if (event.key.code == sf::Keyboard::Right)
+                } else if (event.key.code == sf::Keyboard::Right)
                 {
                     hopper.acceleration.x += 30.0f;
                     hopper.acceleration.y += 70.0f;
-                }
-                else if (event.key.code == sf::Keyboard::Left)
+                } else if (event.key.code == sf::Keyboard::Left)
                 {
                     hopper.acceleration.x += -30.0f;
                     hopper.acceleration.y += 70.0f;
                 }
-            break;
+                break;
             default:
                 break;
         }
@@ -230,7 +219,7 @@ void redrawFrame(sf::RenderWindow &window, const Hopper &hopper, std::vector<Pla
 {
     window.clear(sf::Color(0x00, 0x00, 0x00));
     window.draw(hopper.body);
-    for (Platform &onePlatform : platforms)
+    for (Platform &onePlatform: platforms)
     {
         window.draw(onePlatform.body);
     }
@@ -242,7 +231,6 @@ int main()
     sf::Clock clock;
 
     sf::ContextSettings settings;
-    //вынести в константу
     settings.antialiasingLevel = 8;
     sf::RenderWindow window(
         sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}),
@@ -250,15 +238,17 @@ int main()
 
     Hopper hopper;
     std::vector<Platform> platforms;
+    Platform startPlatform;
 
     sf::Texture hopperTexture;
     initHopper(hopper, hopperTexture, "../assets/crew-dragon.png");
     initPlatforms(platforms);
+    initPlatform(startPlatform, START_PLATFORM_POSITION.x, START_PLATFORM_POSITION.y);
+    platforms.emplace_back(startPlatform);
 
     while (window.isOpen())
     {
         const float deltaTime = clock.restart().asSeconds();
-
         pollEvents(window, hopper);
         updateHopperPosition(deltaTime, hopper, platforms);
         redrawFrame(window, hopper, platforms);
